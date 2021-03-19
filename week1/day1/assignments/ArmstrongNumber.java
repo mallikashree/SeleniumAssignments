6524f405ffddbc0fbdfc36aa124b8db8ddd50aa7
mallikashree <80976562+mallikashree@users.noreply.github.com>
2021-03-19 22:43:06 +0530

    Update ArmstrongNumber.java

--- week1/day1/assignments/ArmstrongNumber.java
+++ week1/day1/assignments/ArmstrongNumber.java
@@ -46,53 +46,36 @@ public class ArmstrongNumber {
 
 		
 
-		// Declare your input
-
-		
-
-		
-
-			// Declare 3 more int variables as calculated, remainder, original (Tip: Initialize calculated alone)
-
-		
-		
-
-			// Assign input into variable original 
-
-		
-
-		
-
-			// Use loop to calculate: use while loop to set condition until the number greater than 0
-
-		
-
-			// Within loop: get the remainder when done by 10 (Tip: Use Mod operator)
-
-			
-
-			// Within loop: get the quotient when done by 10 (Tip: Assign the result to input)
-
-			
-
-			// Within loop: Add calculated with the cube of remainder & assign it to calculated
-
-			
 				
+		int input =153;
 
-			// Check whether calculated and original are same
+	// Declare 3 more int variables as calculated, remainder, original (Tip: Initialize calculated alone)
+		int Cal=0,Rem,Ori;
 
-		
+	// Assign input into variable original 
 
-			//When it matches print it as Armstrong number
+		Ori=input;
+   // Use loop to calculate: use while loop to set condition until the number greater than 0
 
+		while (input < 0) {
+	// Within loop: get the remainder when done by 10 (Tip: Use Mod operator)
 
-		
+			Rem=(input%10);
+	// Within loop: get the quotient when done by 10 (Tip: Assign the result to input)
+            
+			input=input/10;
 
+	// Within loop: Add calculated with the cube of remainder & assign it to calculated
+			Cal=Cal+(Rem*Rem*Rem);
+		}						
 
-
-		
-
+	// Check whether calculated and original are same
+		if (Cal==Ori) {
+			
+		}
+					
+	//When it matches print it as Armstrong number
+		System.out.println("Print the Armstrong number"+Ori);
 		
 
 		
