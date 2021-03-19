a6ab99209857fc16cb46deef01ee0a94a84d11c4
mallikashree <80976562+mallikashree@users.noreply.github.com>
2021-03-19 22:48:04 +0530

    Update RemoveDuplicates.java

--- week1/day2/assignments/mandatory/RemoveDuplicates.java
+++ week1/day2/assignments/mandatory/RemoveDuplicates.java
@@ -16,6 +16,27 @@ public class RemoveDuplicates {
 	 * g) Displaying the String without duplicate words	
 	 */
 
+ String words[] = string.split(" ");  
+          
+        System.out.println("Duplicate words in a given string : ");   
+        for(int i = 0; i < words.length; i++) {  
+            count = 1;  
+            for(int j = i+1; j < words.length; j++) {  
+                if(words[i].equals(words[j])) {  
+                    count++;  
+               
+                }
+            }
+            
+           if (count>1 ) {
+
+               System.out.println(string.replace(words[i], " "));  
+			
+		}
+		
+        }
+
+	}
 
 
 }
