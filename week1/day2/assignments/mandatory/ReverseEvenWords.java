068fc0463ffa342fbd1ccb51843de8b60814b16a
mallikashree <80976562+mallikashree@users.noreply.github.com>
2021-03-19 22:48:56 +0530

    Update ReverseEvenWords.java

--- week1/day2/assignments/mandatory/ReverseEvenWords.java
+++ week1/day2/assignments/mandatory/ReverseEvenWords.java
@@ -21,5 +21,22 @@ public class ReverseEvenWords {
 			
 			 
 		*/
+	
+		String test="I am a software tester";
+		String[] t = test.split(" ");
+
+		String val = "";
+		for (int i = 0; i < t.length; i++) {
+
+		    if (i % 2 == 1)
+		        val = val + new StringBuilder(t[i]).reverse().toString() + " ";
+		    else
+		        val = val + t[i] + " ";
+		}
+		val = val.trim();
+		System.out.println(val);
+
+
+	}
 
 }
